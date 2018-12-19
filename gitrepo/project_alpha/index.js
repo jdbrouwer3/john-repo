d68c830097fccf58eb6bbc32379be9fb1ea314e5
John <jdbrouwer3@gmail.com>
2018-12-19 16:04:36 -0500

    the final project alpha push

--- gitrepo/project_alpha/index.js
+++ gitrepo/project_alpha/index.js
@@ -11,6 +11,31 @@ signUpButton.addEventListener('click', () => {
   });
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // var directAfterSignUp = document.querySelector('#signUpBtn');
 // directAfterSignUp.addEventListener('click', () => {
 //   window.location.replace('login.html');
